7026f62c68154241f1036dd7fd5c1e7bd9916289
NISHANT4510 <nishant4510@gmail.com>
2025-10-06 23:10:21 +0530

    cors updated

--- Server/index.js
+++ Server/index.js
@@ -1,24 +1,41 @@
-const express = require('express')
-const {connect} = require('mongoose')
-require("dotenv").config()
-const cors = require('cors')
-const upload = require('express-fileupload')
-const { notFound, errorHandler } = require('./middleware/errorMiddleware')
-const routes = require('./routes/routes')
-const { server, app } = require('./socket/socket')
+const express = require("express");
+const { connect } = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const upload = require("express-fileupload");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const routes = require("./routes/routes");
+const { server, app } = require("./socket/socket");
 
 // const app = express()
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json({extended:true}))
-app.use(cors({origin:["http://localhost:3001/","http://localhost:5173/","https://chatapp-frontend4510.netlify.app/"]}))
-app.use(upload())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+// app.use(cors({origin:["http://localhost:3001/","http://localhost:5173/","https://chatapp-frontend4510.netlify.app/"]}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:5173",
+      "https://chatapp-frontend4510.netlify.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // only if you use cookies or Authorization headers
+  })
+);
 
+app.options("*", cors()); // handle preflight for all routes
+app.use(upload());
 
-app.use('/api', routes);
+app.use("/api", routes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-connect(process.env.MONGO_URL).then(server.listen(process.env.PORT, () => console.log(`Server is started on port ${process.env.PORT}`))).catch(err => console.log(err))
-
+connect(process.env.MONGO_URL)
+  .then(
+    server.listen(process.env.PORT, () =>
+      console.log(`Server is started on port ${process.env.PORT}`)
+    )
+  )
+  .catch((err) => console.log(err));
